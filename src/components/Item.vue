51fcd763d6c52a1b62ed56921d7ed5529ac7f0ac
Mateo-tech <mat.hladky@gmail.com>
2022-08-07 13:33:05 +0000

    Basic component for Item

---
+++ src/components/Item.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="container">
+		<h4>Item: {{ item.title }}</h4>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		item: {
+			type: Object,
+			required: true
+		}
+	}
+};
+</script>
---
+++ src/views/ItemListView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <div class="d-flex flex-row">
+          <h2>Seznam</h2>
+          <h2>New</h2>
+        </div>
+		<div class="items">
+			<Item v-for="item in items" :key="item.id" :item="item"/>
+		</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from "@/components/Item.vue";
+
+export default {
+  name: "ItemListView",
+  components: {
+    Item
+  },
+  data() {
+    return {
+      items: [
+	 	{
+			id: 1,
+			title: 'Mozarella',
+			bought: false
+		}
+	  ]
+    }
+  }
+};
+</script>
